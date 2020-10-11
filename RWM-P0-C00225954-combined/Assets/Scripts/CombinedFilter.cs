0a5ca25e21a65402aa3ab4ecb147c4ff8a1c7437
Ashleigh <c00225954@itcarlow.ie>
2020-10-12 00:05:55 +0100

    p0

---
+++ RWM-P0-C00225954-combined/Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,8 @@
+﻿
+public class CombinedFilter
+{
+    public static int[] combinedFilter(int[] xs)
+    {
+        return RunningTotal.runTotal(FizzBuzz.FizzBuzzSimple(xs));
+    }
+}
---
+++ RWM-P0-C00225954-combined/Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CombinedFilterTest
+    {
+        [Test]
+        public void CombinedFilterSimple()
+        {
+            int[] input = { 4, 3, 6, 30, 10, 12 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { 4, 7, 10, 10, 15, 18 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
